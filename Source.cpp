cb8276d959cb8d7f2ae0e02eb202d0a739243439
Arnulfo <35495475+sudokufools@users.noreply.github.com>
2018-03-21 08:36:35 -0600

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string adjective[15]{ "Big","Lil","Ice","Tres","Missy","King","G-","Yung","T","$","Notorious","DJ","Dr.","Biggy","Sack" };
+	string noun[20]{"Chainz","Pac","Doom","Mama","Shady","Marley","Thug","Slumpgod","God","Bowser","Ham","Rick"};
+	rand();
+}
